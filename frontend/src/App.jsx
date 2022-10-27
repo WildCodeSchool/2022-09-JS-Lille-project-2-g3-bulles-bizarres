6c6ad3d144e49aef828bfc03edf49a6708738c04
FlorianBartier <florianbartierpro@gmail.com>
2022-10-27 15:22:05 +0200

    Centrage du bouton de recherche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,16 @@ import SearchButton from "@components/SearchButton";
 
 function App() {
   return (
-
     <div className="App">
       <p>coucou</p>
-      
-    <div className="layout">
-      <div className="header"> Header is here!</div>
-      <div className="main">Caroussel is Here! </div>
-      <div className="searchbtn"><SearchButton /></div>
 
+      <div className="layout">
+        <div className="header"> Header is here!</div>
+        <div className="main">Caroussel is Here! </div>
+        <div className="searchbtn">
+          <SearchButton />
+        </div>
+      </div>
     </div>
   );
 }
