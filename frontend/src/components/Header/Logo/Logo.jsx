b13e782ad7f03f2580965937af5a2cb88526c52e
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-14 16:57:47 +0100

    Adding poster path on card

--- frontend/src/components/Header/Logo/Logo.jsx
+++ frontend/src/components/Header/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import "./Logo.modules.css";
 function Logo() {
   return (
     <div className="Logo">
-      <a href="/App.jsx">
+      <a href="/">
         <img src="/src/components/Header/assets/Logo.svg" alt="" />
       </a>
     </div>
